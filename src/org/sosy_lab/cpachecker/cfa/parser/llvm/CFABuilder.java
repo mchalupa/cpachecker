3daec65b54559d92625ba7b1f654bb88cd6e62af
Marek Chalupa <chalupa@fi.muni.cz>
2019-04-10 12:43:06 +0200

    LLVM: give a better exception message
    A follow-up for the previous commit, suggested by @lembergerth.

--- src/org/sosy_lab/cpachecker/cfa/parser/llvm/CFABuilder.java
+++ src/org/sosy_lab/cpachecker/cfa/parser/llvm/CFABuilder.java
@@ -1452,12 +1452,12 @@ public class CFABuilder {
         return new CUnaryExpression(
             location, pExpectedType, expression, UnaryOperator.AMPER);
       } else {
-        throw new AssertionError("Unhandled type structure");
+        throw new AssertionError("Unhandled type structure: " + expressionType);
       }
     } else if (expressionType instanceof CPointerType) {
       return new CPointerExpression(location, pExpectedType, expression);
     } else {
-      throw new AssertionError("Unhandled types structure");
+      throw new AssertionError("Unhandled type structure: " + expressionType);
     }
   }
 
